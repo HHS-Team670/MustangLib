f411f53ae65ad08b7b7cedd14d8e09d1899549c8
ctchen <claire.t.chen.ca@gmail.com>
2019-04-27 15:09:13 -0700

    adding search things

---
+++ MustangLib/src/main/java/frc/team670/robot/utils/search/Node.java
@@ -0,0 +1,18 @@
+package frc.team670.robot.utils.sort;
+
+/**
+* Models a node on a graph
+*/
+public interface Node extends Comparable<Node> {  
+    
+    /**
+     * @return a list of the edges that hit this node
+     */
+    public Edge[] getEdges();
+    
+    /**
+     * @return estimated distance to target node
+     */
+    public int getHeuristicDistance(Node target);
+
+}
