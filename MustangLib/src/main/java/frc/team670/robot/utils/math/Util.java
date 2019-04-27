2ff6ba7b06747cf400d34757a5dc0cf7388e6d0d
ctchen <claire.t.chen.ca@gmail.com>
2019-04-27 15:38:02 -0700

    math (AKA chezy 🅱️oof code) added

---
+++ MustangLib/src/main/java/frc/team670/robot/utils/math/Util.java
@@ -0,0 +1,48 @@
+package frc.team670.robot.utils.math;
+
+import java.util.List;
+
+/**
+ * Taken from Team 254's 2017 robot code.
+ * Contains commonly-used basic functions.
+ */
+public class Util {
+    
+    /** Prevent this class from being instantiated. */
+    private Util() {
+    }
+
+    /**
+     * Limits the given input to the given magnitude.
+     */
+    public static double limit(double v, double maxMagnitude) {
+        return limit(v, -maxMagnitude, maxMagnitude);
+    }
+
+    public static double limit(double v, double min, double max) {
+        return Math.min(max, Math.max(min, v));
+    }
+
+    public static String joinStrings(String delim, List<?> strings) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strings.size(); ++i) {
+            sb.append(strings.get(i).toString());
+            if (i < strings.size() - 1) {
+                sb.append(delim);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static boolean epsilonEquals(double a, double b, double epsilon) {
+        return (a - epsilon <= b) && (a + epsilon >= b);
+    }
+
+    public static boolean allCloseTo(List<Double> list, double value, double epsilon) {
+        boolean result = true;
+        for (Double value_in : list) {
+            result &= epsilonEquals(value_in, value, epsilon);
+        }
+        return result;
+    }
+}
