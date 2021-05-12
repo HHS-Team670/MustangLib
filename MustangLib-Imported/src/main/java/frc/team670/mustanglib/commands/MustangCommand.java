f22d97efe2f2b70309f9f09924fd3324dc00ccb2
Laksh Bhambhani <lakshbh@gmail.com>
2021-05-11 20:42:53 -0700

    minor cleanup

--- MustangLib-Imported/src/main/java/frc/team670/mustanglib/commands/MustangCommand.java
+++ MustangLib-Imported/src/main/java/frc/team670/mustanglib/commands/MustangCommand.java
@@ -30,37 +30,20 @@ public interface MustangCommand{
      * @return A RamseteCommand which will drive the given trajectory
      */
     default RamseteCommand getTrajectoryFollowerCommand(Path path, TankDriveBase driveBase) {
-        // PIDController leftPIDController = new PIDController(RobotConstantsBase.leftKPDriveVel,
-        //         RobotConstantsBase.leftKIDriveVel, RobotConstantsBase.leftKDDriveVel);
-        // PIDController rightPIDController = new PIDController(RobotConstantsBase.rightKPDriveVel,
-        //         RobotConstantsBase.rightKIDriveVel, RobotConstantsBase.rightKDDriveVel);
 
-       
 
-        // RamseteCommand ramseteCommand = new RamseteCommand(path.getTrajectory(), driveBase::getPose,
-        //         new RamseteController(RobotConstantsBase.kRamseteB, RobotConstantsBase.kRamseteZeta),
-        //         new SimpleMotorFeedforward(RobotConstantsBase.leftKsVolts, RobotConstantsBase.leftKvVoltSecondsPerMeter,
-        //                 RobotConstantsBase.leftKaVoltSecondsSquaredPerMeter),
-        //         new SimpleMotorFeedforward(RobotConstantsBase.rightKsVolts, RobotConstantsBase.rightKvVoltSecondsPerMeter,
-        //                 RobotConstantsBase.rightKaVoltSecondsSquaredPerMeter),
-        //         RobotConstantsBase.kDriveKinematics, driveBase::getWheelSpeeds, leftPIDController, rightPIDController,
-        //         driveBase::tankDriveVoltage, driveBase);
-
-        //PIDController leftPIDController = driveBase::getLeftPIDController;
-        //PIDController rightPIDController =  driveBase::getRightPIDController;
-
-RamseteCommand ramseteCommand = new RamseteCommand(
-        path.getTrajectory(), 
-        driveBase::getPose,
-        new RamseteController(RobotConstantsBase.kRamseteB, RobotConstantsBase.kRamseteZeta),
-        driveBase.getLeftSimpleMotorFeedforward(),
-        driveBase.getRightSimpleMotorFeedforward(),
-        driveBase.getKDriveKinematics(), 
-        driveBase::getWheelSpeeds, 
-        driveBase.getLeftPIDController(), 
-        driveBase.getRightPIDController(),
-        driveBase::tankDriveVoltage, 
-        driveBase);
+        RamseteCommand ramseteCommand = new RamseteCommand(
+                path.getTrajectory(), 
+                driveBase::getPose,
+                new RamseteController(RobotConstantsBase.kRamseteB, RobotConstantsBase.kRamseteZeta),
+                driveBase.getLeftSimpleMotorFeedforward(),
+                driveBase.getRightSimpleMotorFeedforward(),
+                driveBase.getKDriveKinematics(), 
+                driveBase::getWheelSpeeds, 
+                driveBase.getLeftPIDController(), 
+                driveBase.getRightPIDController(),
+                driveBase::tankDriveVoltage, 
+                driveBase);
 
         // Reset odometry to the starting pose of the trajectory. We are currently doing
         // this in the command where the trajectory is getting chosen
