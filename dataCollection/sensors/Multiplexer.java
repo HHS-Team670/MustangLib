e62eac2ac80fa3671a7ba89d7e3c72cc8220e35b
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-09-05 15:22:26 -0700

    update multiplexer

--- dataCollection/sensors/Multiplexer.java
+++ dataCollection/sensors/Multiplexer.java
@@ -1,7 +1,6 @@
 package frc.team670.mustanglib.dataCollection.sensors;
 
-import edu.wpi.first.wpilibj.I2C;
-import frc.team670.mustanglib.utils.Logger;
+import edu.wpi.first.wpilibj.DigitalOutput;
 
 /**
  * Adafruit TCA9548A 1-to-8 I2C multiplexer. Based on
@@ -11,62 +10,36 @@ import frc.team670.mustanglib.utils.Logger;
  * @author KatiaBravo
  */
 public class Multiplexer {
-  private I2C multiplexer;
+  /**
+   * Represents inputs to a0, a1, and a2 pins on the multiplexer.
+   * These pins allow switching of multiplexer channels
+   */
+  private DigitalOutput a0, a1, a2;
 
-  private static final int MUX_ADDR = 0x70;
-  private static final int ERROR = 300;
-
-  public Multiplexer(I2C.Port port) {
-    multiplexer = new I2C(port, MUX_ADDR);
+  public Multiplexer() {
+    a0 = new DigitalOutput(3);
+    a1 = new DigitalOutput(4);
+    a2 = new DigitalOutput(5);
   }
 
-  public void selectPort(int address) {
-    assert address <= 7;
-    multiplexer.write(MUX_ADDR, 1 << address);
-  }
-
-  public boolean write(int address, int data) {
-      byte[] rawData = new byte[3];
-
-        rawData[0] = (byte) ((address >> 8) & 0xFF); // MSB of register Address
-        rawData[1] = (byte) (address & 0xFF); // LSB of register address
-        rawData[2] = (byte) data;
-
-        return !multiplexer.writeBulk(rawData, 3);
-  }
-
-  public int read(int address) {
-    byte[] data = new byte[1];
-
-    // This sensor needs 2 bytes so cannot just use read method on I2C class
-    byte[] rawData = new byte[2];
-
-    rawData[0] = (byte) ((address >> 8) & 0xFF); // MSB of register Address
-    rawData[1] = (byte) (address & 0xFF); // LSB of register address
-
-    if (multiplexer.transaction(rawData, 2, data, 1)) {
-        return data[0] & 0xFF;
+  /**
+   * Selects the given port on the multiplexer
+   * @param address The address which should be selected. This should be in the range 0-7
+   */
+  public void select(int address) {
+    // ensure address is between 0-7
+    if(!(address <= 7 && address >= 0)) {
+      return;
     }
-    return ERROR;
-  }
 
-  public void testAddresses() {
-    for (int t=0; t<8; t++) {
-      selectPort(t);
-      Logger.consoleLog("TCA Port #"); 
-      Logger.consoleLog(Integer.toString(t));
-
-      for (int addr = 0; addr<=127; addr++) {
-        if (addr == MUX_ADDR) {
-          continue;
-        }
-
-       
-        if (!write(addr, 0x01)) {
-          Logger.consoleLog("Found I2C 0x");  
-          Logger.consoleLog(Integer.toBinaryString(addr));
-        }
-      }
+    // Convert to binary to get what pins should be enabled
+    String binary = Integer.toBinaryString(address);
+    while(binary.length() < 3) {
+      binary = "0" + binary;
     }
+
+    a0.set(binary.charAt(2) == '1');
+    a1.set(binary.charAt(1) == '1');
+    a2.set(binary.charAt(0) == '1');
   }
 }
