0770a227a0cceabf11c65aae461b28e2fc18f957
Laksh Bhambhani <lakshbh@gmail.com>
2022-01-26 10:03:57 -0800

    MustangCommand modiefied to support ramsetCommand with path and with trajectory

--- commands/MustangCommand.java
+++ commands/MustangCommand.java
@@ -13,18 +13,20 @@ import frc.team670.mustanglib.subsystems.drivebase.DriveBase;
 import frc.team670.mustanglib.path.Path;
 
 /**
- * Represents a robot action with defined health requirements for every subsystem it uses.
+ * Represents a robot action with defined health requirements for every
+ * subsystem it uses.
  * 
  * @author lakshbhambhani
  */
-public interface MustangCommand{
+public interface MustangCommand {
 
     /**
-     * @return A Map containing the minimum health condition for each subsystem that this Command requires to be safely used.
+     * @return A Map containing the minimum health condition for each subsystem that
+     *         this Command requires to be safely used.
      */
     public Map<MustangSubsystemBase, MustangSubsystemBase.HealthState> getHealthRequirements();
 
-       /**
+    /**
      * 
      * @param path      The trajectory to follow
      * @param driveBase
@@ -32,18 +34,46 @@ public interface MustangCommand{
      */
     default RamseteCommand getTrajectoryFollowerCommand(Path path, DriveBase driveBase) {
 
-
         RamseteCommand ramseteCommand = new RamseteCommand(
-                path.getTrajectory(), 
+                path.getTrajectory(),
                 driveBase::getPose,
                 new RamseteController(RobotConstantsBase.kRamseteB, RobotConstantsBase.kRamseteZeta),
                 driveBase.getLeftSimpleMotorFeedforward(),
                 driveBase.getRightSimpleMotorFeedforward(),
-                driveBase.getKDriveKinematics(), 
-                driveBase::getWheelSpeeds, 
-                driveBase.getLeftPIDController(), 
+                driveBase.getKDriveKinematics(),
+                driveBase::getWheelSpeeds,
+                driveBase.getLeftPIDController(),
                 driveBase.getRightPIDController(),
-                driveBase::tankDriveVoltage, 
+                driveBase::tankDriveVoltage,
+                driveBase);
+
+        // Reset odometry to the starting pose of the trajectory. We are currently doing
+        // this in the command where the trajectory is getting chosen
+        // driveBase.resetOdometry(path.getStartingPose());
+
+        // Run path following command, then stop at the end.
+        return ramseteCommand;
+    }
+
+    /**
+     * 
+     * @param path      The trajectory to follow
+     * @param driveBase
+     * @return A RamseteCommand which will drive the given trajectory
+     */
+    default RamseteCommand getTrajectoryFollowerCommand(Trajectory path, DriveBase driveBase) {
+
+        RamseteCommand ramseteCommand = new RamseteCommand(
+                path,
+                driveBase::getPose,
+                new RamseteController(RobotConstantsBase.kRamseteB, RobotConstantsBase.kRamseteZeta),
+                driveBase.getLeftSimpleMotorFeedforward(),
+                driveBase.getRightSimpleMotorFeedforward(),
+                driveBase.getKDriveKinematics(),
+                driveBase::getWheelSpeeds,
+                driveBase.getLeftPIDController(),
+                driveBase.getRightPIDController(),
+                driveBase::tankDriveVoltage,
                 driveBase);
 
         // Reset odometry to the starting pose of the trajectory. We are currently doing
