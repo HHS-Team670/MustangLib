b5702ffb7e9655aa7ee7e664adb6cc88d781af2e
Laksh Bhambhani <lakshbh@gmail.com>
2022-02-20 20:59:51 -0800

    warnings fixed, vision turnedon logic changed

--- commands/vision/SetVisionLEDs.java
+++ commands/vision/SetVisionLEDs.java
@@ -10,16 +10,13 @@ package frc.team670.mustanglib.commands.vision;
 import java.util.Map;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-// import frc.team670.robot.RobotContainer;
 import frc.team670.mustanglib.commands.MustangCommand;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
-import frc.team670.mustanglib.subsystems.VisionSubsystemBase;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
-import frc.team670.mustanglib.utils.Logger;
-import frc.team670.robot.subsystems.Vision;
+import frc.team670.mustanglib.subsystems.VisionSubsystemBase;
 
 /**
- * Flips the direction of the drive: forward or reversed.
+ * Set vision leds to turned on or off
  */
 public class SetVisionLEDs extends InstantCommand implements MustangCommand {
 
