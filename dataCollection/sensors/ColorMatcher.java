2c796c470688d3d8b5ffe1033419373aebc6ab4c
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2022-03-29 21:48:36 -0700

    add both color matcher types

--- dataCollection/sensors/ColorMatcher.java
+++ dataCollection/sensors/ColorMatcher.java
@@ -3,19 +3,22 @@ package frc.team670.mustanglib.dataCollection.sensors;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
-import frc.team670.mustanglib.dataCollection.sensors.PicoColorSensor.RawColor;
 
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorMatch;
 
-public class PicoColorMatcher {
+public class ColorMatcher {
+  /**
+   * Change the I2C port below to match the connection of your color sensor
+   */
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
 
   /**
    * A Rev Color Sensor V3 object is constructed with an I2C port as a parameter.
    * The device will be automatically initialized with default parameters.
    */
-  private final PicoColorSensor m_colorSensor = new PicoColorSensor();
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
   /**
    * A Rev Color Match object is used to register and detect known colors. This
@@ -28,8 +31,8 @@ public class PicoColorMatcher {
 
   public enum colors {
 
-    BLUE(0, new Color(0.09, 0.04, 0.10)), // 2022 blue game piece
-    RED(2, new Color(0.10, 0.03, 0.09)); // 2022 red game piece
+    BLUE(0, new Color(0.136, 0.412, 0.450)), // 2022 blue game piece
+    RED(1, new Color(0.475, 0.371, 0.153)); 
 
     private int colorNumber;
     private Color color;
@@ -56,12 +59,12 @@ public class PicoColorMatcher {
 
   private final double CONFIDENCE_THRESHOLD = 0.85;
 
-  public PicoColorMatcher() {
+  public ColorMatcher() {
     init();
   }
 
   public void init() {
-    m_colorMatcher.addColorMatch(colors.BLUE.getTargetColor());;
+    m_colorMatcher.addColorMatch(colors.BLUE.getTargetColor());
     m_colorMatcher.addColorMatch(colors.RED.getTargetColor());
 
     m_colorMatcher.setConfidenceThreshold(CONFIDENCE_THRESHOLD);
@@ -78,7 +81,7 @@ public class PicoColorMatcher {
      * is the more light from the surroundings will bleed into the measurements and
      * make it difficult to accurately determine its color.
      */
-    RawColor detectedColor = m_colorSensor.getRawColor0();
+    Color detectedColor = m_colorSensor.getColor();
 
     /**
      * Run the color match algorithm on our detected color
@@ -86,12 +89,11 @@ public class PicoColorMatcher {
     // String colorString;
     int colorNumber;
 
-    ColorMatchResult match = m_colorMatcher.matchClosestColor(new Color(detectedColor.red/100.0, detectedColor.green/100.0, detectedColor.blue/100.0));
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(new Color(detectedColor.red, detectedColor.green, detectedColor.blue));
     SmartDashboard.putNumber("Red", detectedColor.red);
     SmartDashboard.putNumber("Green", detectedColor.green);
     SmartDashboard.putNumber("Blue", detectedColor.blue);
     SmartDashboard.putNumber("Confidence", match.confidence);
-    SmartDashboard.putBoolean("CS: sensor is connected", m_colorSensor.isSensor0Connected());
     if(match.confidence >= CONFIDENCE_THRESHOLD) {
         if (match.color == colors.BLUE.getTargetColor()) {
         //   coaalorString = "Blue";
