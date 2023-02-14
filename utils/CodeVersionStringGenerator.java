befe894147c7fbc752e667e7d27dd7349cec2214
Alex Sirota <67526318+ajs256@users.noreply.github.com>
2023-02-13 18:58:26 -0800

    Add `CodeVersionStringGenerator`

---
+++ utils/CodeVersionStringGenerator.java
@@ -0,0 +1,17 @@
+package frc.team670.mustanglib.utils;
+
+import frc.team670.robot.constants.CodeVersion;
+
+public final class CodeVersionStringGenerator {
+    public static String genVersionString() {
+        String str = "";
+
+        if (CodeVersion.DIRTY == 1) {
+            str = CodeVersion.GIT_BRANCH + " (dirty) at " + CodeVersion.BUILD_DATE;
+        } else {
+            str = CodeVersion.GIT_BRANCH + "_" + CodeVersion.BUILD_DATE + "_" + CodeVersion.GIT_SHA.substring(0, 8);
+        }
+
+        return str;
+    }
+}
