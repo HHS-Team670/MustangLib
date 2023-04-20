325b474deb2a1eb98a6aee3cb1500765a37859fc
Ethan <ethanytchang@gmail.com>
2023-04-20 10:54:19 -0700

    refactor led color

--- subsystems/LEDSubsystem.java
+++ subsystems/LEDSubsystem.java
@@ -2,7 +2,6 @@ package frc.team670.mustanglib.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import frc.team670.mustanglib.utils.LEDColor;
 
 /**
  * Represents an addresable LED strip
@@ -252,6 +251,62 @@ public abstract class LEDSubsystem extends MustangSubsystemBase {
     public void setBuffer(AddressableLEDBuffer buffer) {
         this.m_ledBuffer = buffer;
         changed = true;
+    }
+
+    /**
+     * Represents colors that our LED subsystem supports
+     * Extends WPILib Color
+     * Stores Colors in hsv format
+     * 
+     */
+    public static class LEDColor {
+        public static final LEDColor RED = new LEDColor(0);
+
+        public static final LEDColor SPOOKY_ORANGE = new LEDColor(3);
+
+        public static final LEDColor YELLOW = new LEDColor(15);
+
+        public static final LEDColor GREEN = new LEDColor(60);
+
+        public static final LEDColor LIGHT_BLUE = new LEDColor(85);
+
+        public static final LEDColor BLUE = new LEDColor(120);
+
+        public static final LEDColor PURPLE = new LEDColor(140);
+
+        public static final LEDColor PINK = new LEDColor(169);
+
+        public static final LEDColor WHITE = new LEDColor(0, 0);
+
+        public static final LEDColor SEXY_PURPLE = new LEDColor(137);
+
+        public static final LEDColor SEXY_YELLOW = new LEDColor(12);
+
+        public final int h, s, v;
+
+        public LEDColor(int h, int s, int v) {
+            this.h = h;
+            this.s = s;
+            this.v = v;
+
+        }
+
+        public LEDColor(int h) {
+            this(h, 255, 255);
+
+        }
+
+        public LEDColor(int h, int s) {
+            this(h, s, 255);
+        }
+
+        public LEDColor dimmer() {
+            return new LEDColor(h, s, v / 3);
+        }
+
+        public LEDColor brighter() {
+            return new LEDColor(h, s, v * 3);
+        }
     }
 
 }
