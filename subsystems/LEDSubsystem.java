98e8e4dca1986884d8322c6530202b6e51f9198a
Aditi Kattuparambil <112968168+KattuparambilAditi@users.noreply.github.com>
2023-10-25 10:45:43 -0700

    more unscuff + instructions

--- subsystems/LEDSubsystem.java
+++ subsystems/LEDSubsystem.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-public class LEDSubsystem extends MustangSubsystemBase {
+public abstract class LEDSubsystem extends MustangSubsystemBase {
     /**edu.wpi.first.wpilibj.examples.addressableled
      * The default duration of a blink
      */
@@ -51,23 +51,16 @@ public class LEDSubsystem extends MustangSubsystemBase {
         m_led.setData(m_ledBuffer);
         m_led.start();
     }
-
-    public void mustangPeriodic() {
-       // Fill the buffer with a rainbow
-        rainbow();
-        // Set the LEDs
-        m_led.setBuffer(m_ledBuffer);
-    }
-
-    @Override
-    public void debugSubsystem() {
-    }
-
-    @Override
-    public HealthState checkHealth() {
-        return HealthState.GREEN;
-    }
-
+    /**
+     * Instructions:
+     * make a state enum for rainbow, blinking and solid/not moving
+     * make setters for all enums (solid in solid hsv)
+     * use similar the same logic that we used for the claw, only calling rainbow after a certain amount of cycles
+     * ( we do this because we do not want to overload the robot by calling a method every .2 seconds when we could just do it every 3-5)
+     * make a blinking method (could also just switch between solid hsv and off)
+     * If you finish and it builds talk to me and we can test it out
+     * Once your code works you can add whatever new methods and colors
+     */
 
     public void rainbow() {
         // For every pixel
@@ -103,6 +96,22 @@ public class LEDSubsystem extends MustangSubsystemBase {
 
     }
 
+    public void mustangPeriodic() {
+       // Fill the buffer with a rainbow
+        rainbow();
+        // Set the LEDs
+        m_led.setData(m_ledBuffer);
+    }
+
+    @Override
+    public void debugSubsystem() {
+    }
+
+    @Override
+    public HealthState checkHealth() {
+        return HealthState.GREEN;
+    }
+
     /**
      * Sets the led buffer
      * 
@@ -110,7 +119,7 @@ public class LEDSubsystem extends MustangSubsystemBase {
      */
     public void setBuffer(AddressableLEDBuffer buffer) {
         this.m_ledBuffer = buffer;
-\    }
+   }
 
     /**
      * Represents colors that our LED subsystem supports
