37a5fc328ca9b4cda1a28e0c6b60372af297e759
Armaan Gomes <acgo8888@gmail.com>
2024-02-29 16:28:31 -0800

    loop spark max settings

--- utils/motorcontroller/SparkMAXFactory.java
+++ utils/motorcontroller/SparkMAXFactory.java
@@ -124,10 +124,12 @@ public class SparkMAXFactory {
     public static SparkMAXLite buildSparkMAX(int deviceID, Config config, MotorConfig.Motor_Type motorType) {
         SparkMAXLite sparkMax = new SparkMAXLite(deviceID, motorType);
         sparkMax.restoreFactoryDefaults();
-        sparkMax.set(ControlType.kDutyCycle, 0);
-        sparkMax.setInverted(config.INVERTED);
-        sparkMax.setSmartCurrentLimit(MotorConfig.MOTOR_MAX_CURRENT.get(motorType));
-        sparkMax.enableVoltageCompensation(12);
+        for(int i=0;i<30;i++){
+            sparkMax.set(ControlType.kDutyCycle, 0);
+            sparkMax.setInverted(config.INVERTED);
+            sparkMax.setSmartCurrentLimit(MotorConfig.MOTOR_MAX_CURRENT.get(motorType));
+            sparkMax.enableVoltageCompensation(12);
+        
         sparkMax.setPeriodicFramePeriod(PeriodicFrame.kStatus0, config.STATUS_FRAME_0_RATE_MS);
         sparkMax.setPeriodicFramePeriod(PeriodicFrame.kStatus1, config.STATUS_FRAME_1_RATE_MS);
         sparkMax.setPeriodicFramePeriod(PeriodicFrame.kStatus2, config.STATUS_FRAME_2_RATE_MS);
@@ -135,7 +137,7 @@ public class SparkMAXFactory {
         sparkMax.setPeriodicFramePeriod(PeriodicFrame.kStatus4, config.STATUS_FRAME_4_RATE_MS);
         sparkMax.setPeriodicFramePeriod(PeriodicFrame.kStatus5, config.STATUS_FRAME_5_RATE_MS);
         sparkMax.setPeriodicFramePeriod(PeriodicFrame.kStatus6, config.STATUS_FRAME_6_RATE_MS);
-
+        }
         return sparkMax;
     }
 
