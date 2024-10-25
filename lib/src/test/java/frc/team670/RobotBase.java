d0e8603064c2f0bf3f82001ee4824af7faab22b2
Yim Hoseon <justin.yim14@gmail.com>
2024-10-24 19:30:58 -0700

    feat: moved to gradle project

---
+++ lib/src/test/java/frc/team670/RobotBase.java
@@ -0,0 +1,218 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.team670.mustanglib;
+
+import java.util.Objects;
+
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+// import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import frc.team670.mustanglib.commands.MustangCommand;
+import frc.team670.mustanglib.commands.MustangScheduler;
+import frc.team670.mustanglib.utils.ConsoleLogger;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class RobotBase extends LoggedRobot {
+
+  private MustangCommand m_autonomousCommand;
+
+  private Timer timer;
+  private double SYSTEM_CHECK_PERIOD = 5;
+
+  private static final double startTimeMillis = System.currentTimeMillis();
+
+  private static String timeSinceStartupAsString = "00:00:00.000";
+
+  private static RobotBase instance;
+
+  public static boolean overrideAtCompetition = true;
+
+  RobotContainerBase robotContainer;
+
+  public RobotBase(RobotContainerBase robotContainer) {
+    this.robotContainer = robotContainer;
+    RobotBase.instance = this;
+  }
+
+  public RobotContainerBase getRobotContainer() {
+    return robotContainer;
+  }
+
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    Logger.recordMetadata("ProjectName", "670-Robot"); // Set a metadata value
+
+    if (isReal()) {
+        Logger.addDataReceiver(new WPILOGWriter("/U/logs")); // Log to a USB stick
+        Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+    } else {
+        setUseTiming(false); // Run as fast as possible
+        String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+        Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+        Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+    }
+    
+    // Logger.disableDeterministicTimestamps() // See "Deterministic Timestamps" in the "Understanding Data Flow" page
+    Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added
+    // Instantiate our RobotContainer. This will perform all our button bindings
+    RobotContainerBase.checkSubsystemsHealth();
+    timer = new Timer();
+    timer.start();
+    robotContainer.robotInit();
+
+    MustangScheduler.getInstance();
+
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
+   *
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
+   * 
+   * Re-calculates the health of all subsystems on the robot at specified
+   * intervals.
+   */
+  @Override
+  public void robotPeriodic() {
+    MustangScheduler.getInstance().run();
+    robotContainer.periodic();
+    updateTimeSinceStartupAsString();
+    if (timer.hasElapsed(SYSTEM_CHECK_PERIOD)) {
+      RobotContainerBase.checkSubsystemsHealth();
+    }
+  }
+
+  private void updateTimeSinceStartupAsString() {
+    int millisSinceStartup = (int) (System.currentTimeMillis() - startTimeMillis);
+    int secondsSinceStartup = (int) (millisSinceStartup / 1000);
+    int sec = secondsSinceStartup % 60;
+    int min = (secondsSinceStartup / 60) % 60;
+    int hours = (secondsSinceStartup / 60) / 60;
+    int milliseconds = millisSinceStartup - ((millisSinceStartup / 1000) * 1000);
+    String strSec = (sec < 10) ? "0" + Integer.toString(sec) : Integer.toString(sec);
+    String strmin = (min < 10) ? "0" + Integer.toString(min) : Integer.toString(min);
+    String strHours = (hours < 10) ? "0" + Integer.toString(hours) : Integer.toString(hours);
+    String strMillis = (milliseconds < 100)
+        ? "0" + ((milliseconds < 10) ? "0" + milliseconds : Integer.toString(milliseconds))
+        : Integer.toString(milliseconds);
+    timeSinceStartupAsString = strHours + ":" + strmin + ":" + strSec + "." + strMillis;
+  }
+
+  public static String getTimeSinceStartup() {
+    return timeSinceStartupAsString;
+  }
+
+  public static synchronized RobotBase getInstance() {
+    return Objects.requireNonNull(instance);
+  }
+
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   */
+  @Override
+  public void disabledInit() {
+    robotContainer.disabled();
+  }
+
+  @Override
+  public void disabledPeriodic() {
+    robotContainer.disabledPeriodic();
+  }
+
+  /**
+   * This autonomous runs the selected Autonomous Command
+   */
+  @Override
+  public void autonomousInit() {
+    ConsoleLogger.consoleLog("Autonomous Init");
+    robotContainer.autonomousInit();
+    m_autonomousCommand = robotContainer.getAutonomousCommand();
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      MustangScheduler.getInstance().schedule(m_autonomousCommand);
+    }
+
+  }
+
+  @Override
+  public void autonomousExit() {
+    robotContainer.autonomousExit();
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    MustangScheduler.getInstance().run();
+    robotContainer.autonomousPeriodic();
+  }
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      MustangScheduler.getInstance().cancel((MustangCommand) (m_autonomousCommand));
+    }
+    ConsoleLogger.consoleLog("Teleop Init");
+    robotContainer.teleopInit();
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+    MustangScheduler.getInstance().run();
+    robotContainer.teleopPeriodic();
+
+  }
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    MustangScheduler.getInstance().cancelAll();
+    ConsoleLogger.consoleLog("Test Init");
+    LiveWindow.setEnabled(false);
+    robotContainer.testInit();
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+    MustangScheduler.getInstance().run();
+  }
+
+}
---
+++ lib/src/test/java/frc/team670/RobotConstantsBase.java
@@ -0,0 +1,118 @@
+package frc.team670.mustanglib;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import com.pathplanner.lib.util.PIDConstants;
+
+import edu.wpi.first.math.controller.PIDController;
+/**
+ * A place for storing constants required in mustanglib. 
+ * This includes most drivebase constants. For every robot, add mac address like below as well as relevant constants
+ * 
+ * Update every year
+ */
+public class RobotConstantsBase {
+     
+    public  static class SwerveDriveBase {
+        
+        //Front Left
+        public static final int kFrontLeftModuleSteerMotorID = 20;
+        public static final int kFrontLeftModuleDriveMotorID = 21;
+        public static final int kFrontLeftModuleSteerEncoderID = 30;
+        //Front Right
+        public static final int kFrontRightModuleSteerMotorID = 22;
+        public static final int kFrontRightModuleDriveMotorID = 23;
+        public static final int kFrontRightModuleSteerEncoderID = 32;
+       //Back Right
+        public static final int kBackRightModuleSteerMotorID = 24;
+        public static final int kBackRightModuleDriveMotorID = 25;
+        public static final int kBackRightModuleSteerEncoderID = 34;
+        //Back Left
+        public static final int kBackLeftModuleSteerMotorID = 26;
+        public static final int kBackLeftModuleDriveMotorID = 27;
+        public static final int kBackLeftModuleSteerEncoderID = 36;
+        
+        public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI * 8;//What if we turn this up?  //Not robot specific
+        public static final double kMaxAngularAccelerationRadiansPerSecondSquared = Math.PI * 16;// Not robot specific
+
+        public static final double kMaxVoltage = 12.0;// Good defaults
+        public static final double kMaxDriveCurrent = 60.0;//Defaults
+        public static final double kMaxSteerCurrent = 20.0;//Defaults
+
+        public static final double kCurrentSampleTimeRange = 5;
+        public static final int kCurrentSampleSize = (int) (kCurrentSampleTimeRange * 50); // mustangPeriodic runs 50hz
+        public static final double kTotalDriveCurrentThreshold = 120; // 30 per motor
+        public static final double kReducedDriveCurrentLimit = 20; // single motor 
+
+        // The formula for calculating the theoretical maximum velocity is:
+        // <Motor free speed RPM> / 60 * <Drive reduction> * <Wheel diameter meters> *
+        // pi
+        // An example of this constant for a Mk4 L2 module with NEOs to drive is:
+        // 5880.0 / 60.0 / SdsModuleConfigurations.MK4_L2.getDriveReduction() *
+        // SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI
+        
+        // Good defaults
+        public static final PIDConstants kAutonTranslationPID = new PIDConstants(4, 0, 0);
+        public static final PIDConstants kAutonThetaPID = new PIDConstants(1.75, 0, 0);
+
+        // PID controllers
+        public static final PIDController xController = new PIDController(3, 0, 0);
+        public static final PIDController yController = new PIDController(3, 0, 0);
+        public static final PIDController thetaController = new PIDController(0.2, 0, 0);
+      
+    }
+    public static  class TankDriveBase{
+        public static final int kLeftLeaderSparkMotorID = 21; 
+        public static final int kLeftFollowerSparkMotorID = 22;
+        public static final int kRightLeaderSparkMotorID = 23;
+        public static final int kRightFollowerSparkMotorID = 24;
+        /**
+       * The number of ticks per rotation of a drivebase wheel for the SPARK Encoders
+       */
+        public static final int kSparkTicksPerRotation = 1024;//Not RS
+    // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+        public static final double kRamseteB = 2;
+        public static final double kRamseteZeta = .7;
+    
+      
+
+  }
+
+    /**
+     * This is code from Poofs 2022
+     * https://github.com/Team254/FRC-2022-Public/blob/6a24236b37f0fcb75ceb9d5dec767be58ea903c0/src/main/java/com/team254/frc2022/Constants.java#L429 
+     * @return the MAC address of the robot
+     */
+    public static String getMACAddress() {
+        try {
+            
+            Enumeration<NetworkInterface> nwInterface = NetworkInterface.getNetworkInterfaces();
+            StringBuilder ret = new StringBuilder();
+            while (nwInterface.hasMoreElements()) {
+                NetworkInterface nis = nwInterface.nextElement();
+                System.out.println("NIS: " + nis.getDisplayName());
+                if (nis != null && "eth0".equals(nis.getDisplayName())) {
+                    byte[] mac = nis.getHardwareAddress();
+                    if (mac != null) {
+                        for (int i = 0; i < mac.length; i++) {
+                            ret.append(String.format("%02X%s", mac[i],
+                                    (i < mac.length - 1) ? ":" : ""));
+                        }
+                        String addr = ret.toString();
+                        System.out.println("NIS " + nis.getDisplayName() + " addr: " + addr);
+                        return addr;
+                    } else {
+                        System.out.println("Address doesn't exist or is not accessible");
+                    }
+                } else {
+                    System.out.println("Skipping adaptor: " + nis.getDisplayName());
+                }
+            }
+        } catch (SocketException | NullPointerException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\n\nMAC ADDRESS NOTHING");
+        return "";
+    }
+}
