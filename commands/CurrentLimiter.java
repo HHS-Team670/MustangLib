7ababc0eb5ff8719a1acf36ca62a1a94cfdb84f2
NotAPokemon <92272566+NotAPokemon@users.noreply.github.com>
2024-11-07 19:25:54 -0800

    added comments to explain difficult parts of code aswell renamed few variable names

--- commands/CurrentLimiter.java
+++ commands/CurrentLimiter.java
@@ -2,10 +2,13 @@ package frc.team670.mustanglib.commands;
 
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 
+/**
+ * stores the subsytem and speed limit for subsytem in RPM used for DefenseModeCommand
+ */
 public class CurrentLimiter {
     public MustangSubsystemBase subsytem;
     public int limit;
-    public int orginalLimit;
+
     public CurrentLimiter(MustangSubsystemBase subsystem, int limit){
         this.subsytem = subsystem;
         this.limit = limit;
--- commands/DefenseModeCommand.java
+++ commands/DefenseModeCommand.java
@@ -13,7 +13,7 @@ import frc.team670.mustanglib.utils.motorcontroller.SparkMAXLite;
 
 public class DefenseModeCommand extends InstantCommand implements MustangCommand  {
     List<CurrentLimiter> limiter;
-    List<SparkMAXLite> list = new ArrayList<SparkMAXLite>();
+    List<SparkMAXLite> motorList = new ArrayList<SparkMAXLite>();
     List<Integer> speed = new ArrayList<Integer>();
     List<MustangSubsystemBase> testsubs = new ArrayList<MustangSubsystemBase>();
     Map<MustangSubsystemBase, HealthState> healthreq;
@@ -27,17 +27,22 @@ public class DefenseModeCommand extends InstantCommand implements MustangCommand
     }
 
     public DefenseModeCommand(List<MustangSubsystemBase> unusedSubsytems, int constLimit){
+        
+        //creates a CurrentLimiter using the paramiters pased to later be used in the config
         for (int i = 0; i < unusedSubsytems.size(); i++){
             limiter.set(i, new CurrentLimiter(unusedSubsytems.get(i), constLimit));
         }
+
         config();
     }
 
     void config(){
         for (int i = 0; i < limiter.size(); i++){
+            // loop through subsytems and creates a list of their motors and a list of speeds for the motors
+            // to be used when command is called
             SparkMAXLite[] motors = limiter.get(i).subsytem.getMotors();
             for (SparkMAXLite sparkMAXLite : motors) {
-                list.add(sparkMAXLite);
+                motorList.add(sparkMAXLite);
                 speed.add(limiter.get(i).limit);
             }
         }
@@ -46,8 +51,8 @@ public class DefenseModeCommand extends InstantCommand implements MustangCommand
     
     @Override
     public void initialize(){
-        for (int i = 0; i < list.size(); i++) {
-            list.get(i).set(speed.get(i));
+        for (int i = 0; i < motorList.size(); i++) {
+            motorList.get(i).set(speed.get(i));
         }
     }
 
