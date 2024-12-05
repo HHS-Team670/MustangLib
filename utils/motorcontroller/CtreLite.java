c032b571c5ad72c7dff27f6e6152e34b8e74d516
Yim Hoseon <justin.yim14@gmail.com>
2024-12-04 17:10:21 -0800

    feat: Add Ctre Factory and Lite

---
+++ utils/motorcontroller/CtreLite.java
@@ -0,0 +1,89 @@
+package frc.team670.mustanglib.utils.motorcontroller;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.REVLibError;
+
+/**
+ * Wrapper class for a SparkMAX for reducing CAN bus overhead by skipping
+ * duplicate set commands.
+ * 
+ * @author ctychen, lakshbhambhani
+ */
+public final class CtreLite extends CANSparkMax {
+
+    protected double lastSet = Double.NaN;
+    protected ControlType lastControlType = null;
+    protected CANSparkMax leader = null;
+    protected MotorConfig.Motor_Type motorType = null;
+
+    /**
+     * Creates a SparkMAX on a given ID, which is controlling a specified kind of motor.
+     * 
+     * @param type type Which specific motor this controller will
+     *                               be using. For example, NEO or BAG
+     */
+    public CtreLite(int id, MotorConfig.Motor_Type type) {
+        super(id, MotorConfig.MOTOR_TYPE.get(type));
+        this.motorType = type;
+    }
+
+    public double getLastSet() {
+        return this.lastSet;
+    }
+
+    public ControlType getLastControlType() {
+        return this.lastControlType;
+    }
+
+    public MotorConfig.Motor_Type getMotor(){
+        return this.motorType;
+    }
+
+    /**
+     * Applicable if this SparkMAX is set as a follower.
+     * 
+     * @return the 'leader' controller that the SparkMAX follows
+     */
+    public CANSparkMax getLeader() {
+        return this.leader;
+    }
+
+    /**
+     * @param leader the SparkMAX for this controller to follow, if applicable.
+     */
+    public void setFollow(CANSparkMax leader) {
+        this.leader = leader;
+        super.follow(leader);
+    }
+
+    /**
+     * @param kdutycycle mode for this motor controller
+     * @param value      value output of the controller, for the appropriate mode
+     */
+    public void set(ControlType kdutycycle, double value) {
+        if (value != lastSet || kdutycycle != lastControlType) {
+            if(super.getPIDController().setReference(value, kdutycycle) == REVLibError.kOk){
+                this.lastSet = value;
+                this.lastControlType = kdutycycle;
+            }
+        }
+    }
+
+    public void set(double value) {
+        if (value != lastSet) {
+            this.lastSet = value;
+            super.set(value);
+        }
+    }
+
+    /**
+     * 
+     * @return true if there is an issue with this SparkMax, false if the SparkMax
+     *         is connected successfully and without errors.
+     */
+    public boolean isErrored() {
+        return (this == null || this.getLastError() != REVLibError.kOk);
+    }
+
+
+}
